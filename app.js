5e5eefaf864ba5a40c82654fd8f33b6599d37ad3
deveshyadav <dd4168@srmist.edu.in>
2025-06-24 00:50:15 +0530

    1st instance created

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
 const express = require("express");
 const connectDB = require("./config/database");
+const User = require("./models/user");
 const app = express();
 
+//creating APIs
+
+app.post("/signup", async (req, res) => {
+  //create a new Instance for the User model
+  const user = new User({
+    firstName: "Devesh",
+    lastName: "Yadav",
+    emailId: "sher@gmail.com",
+    password: "Sher@123",
+  });
+  try {
+    await user.save();
+    res.send("User saved successfully ;)");
+  } catch (error) {
+    console.log(`Error saving user in db, ${error}`);
+  }
+});
+
+//calling the connectDB fn from db.js
 connectDB()
   .then(() => {
-    console.log("DB connected successfully . . .");
+    console.log("DB connected successfully");
     //after connecting to db, server will run (good approach)
-    app.listen(4000, () => {
-      console.log("Server listening on 7 7 7 7");
+    app.listen(7777, () => {
+      console.log("Server is running");
     });
   })
   .catch((err) => {
