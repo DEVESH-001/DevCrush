1967a611aafc24f8b02c731164c9d22831b56bb3
deveshyadav <dd4168@srmist.edu.in>
2025-07-26 20:46:59 +0530

    Added Request feature in UI

--- frontend/dev/src/components/Request.jsx
+++ frontend/dev/src/components/Request.jsx
@@ -2,13 +2,29 @@
 import axios from "axios";
 import { BASE_URL } from "../utils/constants";
 import { useDispatch, useSelector } from "react-redux";
-import { addRequest } from "../utils/requestSlice";
+import { addRequest, removeRequest } from "../utils/requestSlice";
 import { useEffect } from "react";
-import UserCard from "./ui/UserCard";
 
 const Request = () => {
   const requests = useSelector((store) => store.requests); //useSelector is used to access the Redux store state, specifically the requests state managed by requestSlice
   const dispatch = useDispatch();
+
+  const reviewRequest = async (status, _id) => {
+    try {
+      const res = await axios.post(
+        BASE_URL + "/request/review/" + status + "/" + _id,
+        {},
+        { withCredentials: true }
+      )
+      dispatch(removeRequest(_id)); 
+      console.log("res" + res);
+      // Refetch requests to update UI
+      await fetchRequest();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   const fetchRequest = async () => {
     try {
       const res = await axios.get(BASE_URL + "/user/requests/received", {
@@ -16,7 +32,8 @@ const Request = () => {
       });
       dispatch(addRequest(res.data.data)); //dispatch is used to send the action to the Redux store, which will update the state with the new request data
     } catch (error) {
-      alert(error);
+      console.log(error);
+      
     }
   };
 
@@ -29,12 +46,20 @@ const Request = () => {
 
   return (
     <div>
-      {requests.map((request, index) => {
-        const { firstname, lastname, age, photoUrl, gender, skills, about } =
-          request.fromUserId;
+      {requests.map((request) => {
+        const {
+          _id,
+          firstname,
+          lastname,
+          age,
+          photoUrl,
+          gender,
+          skills,
+          about,
+        } = request.fromUserId;
         return (
           <div
-            key={index}
+            key={_id}
             className="m-4 p-4 border rounded-xl border-gray-500 flex flex-col md:flex-row items-start gap-6"
           >
             <img
@@ -54,8 +79,22 @@ const Request = () => {
               </p>
             </div>
             <div className="cursor-pointer gap-4 font-bold">
-              <button className="bg-blue-600 p-2 m-2">Accept</button>
-              <button className="bg-red-600 p-2 m-2">Reject</button>
+              <button
+                className="bg-blue-600 p-2 m-2"
+                onClick={() =>
+                  reviewRequest("accepted", request.fromUserId._id)
+                } //accepted is the status to be sent to the backend when the request is accepted
+              >
+                Accept
+              </button>
+              <button
+                className="bg-red-600 p-2 m-2"
+                onClick={() =>
+                  reviewRequest("rejected", request.fromUserId._id)
+                }
+              >
+                Reject
+              </button>
             </div>
           </div>
         );
--- frontend/dev/src/utils/requestSlice.js
+++ frontend/dev/src/utils/requestSlice.js
@@ -5,8 +5,13 @@ const requestSlice = createSlice({
   initialState: null,
   reducers: {
     addRequest: (state, action) => action.payload,
+    removeRequest: (state, action) => {
+      // state ? state.filter(req => req.fromUserId._id !== action.payload) : state,
+      const newArray = state.filter((req) => req._id === action.payload); 
+      return newArray;
+    },
   },
 });
 
-export const { addRequest } = requestSlice.actions;
+export const { addRequest, removeRequest } = requestSlice.actions;
 export default requestSlice.reducer;
